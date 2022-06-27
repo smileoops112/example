f92bfb5c0b99a98320e8bd73d34b982df6daaa94
smileoops <dinisa2010@gmail.com>
2022-06-27 15:02:57 +0300

    add new model Rating and changed model Book

--- book_app/admin.py
+++ book_app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Book, Writer
 
-# Register your models here.
+
+admin.site.register(Writer)
+
+
+class RatingFilter(admin.SimpleListFilter):
+
+    title = 'Сортировка по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'низкий'),
+            ('от 40 до 59', 'средний'),
+            ('от 60 до 100', 'высокий'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=59)
+        if self.value() == 'от 60 до 100':
+            return queryset.filter(rating__gte=60).filter(rating__lte=100)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = ['title', 'rating', 'choice', 'author', 'rating_status']
+    list_editable = ['rating', 'choice']
+    ordering = ['rating']
+    list_per_page = 6
+    search_fields = ['title']
+    list_filter = [RatingFilter]
+
+    @admin.display(description='Статус', ordering='rating')
+    def rating_status(self, book: Book):
+        if book.rating < 50:
+            return 'Зачем такое читать?!'
+        if book.rating < 70:
+            return "На разок"
+        return 'То что надо'
--- book_app/models.py
+++ book_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.template.defaultfilters import slugify as django_slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 alphabet = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
             'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
@@ -15,17 +16,33 @@ def slugify(s):
     return django_slugify(''.join(alphabet.get(w, w) for w in s.lower()))
 
 
+class Writer(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
 class Book(models.Model):
 
+    HRN = 'грн'
+    USD = 'дол'
+    EUR = 'евр'
+    CHOICE_PRICE = [
+        (HRN, 'грн'),
+        (EUR, 'евро'),
+        (USD, 'доллары')
+    ]
+
     title = models.CharField(max_length=70)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
     is_best_selling = models.BooleanField()
     author = models.CharField(max_length=40, null=True)
+    choice = models.CharField(max_length=3, choices=CHOICE_PRICE, default=HRN)
     slug = models.SlugField(default='', null=False, db_index=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Book, self).save(*args, **kwargs)
 
     def get_url(self):
         return reverse('book-info', args=[self.slug, ])
